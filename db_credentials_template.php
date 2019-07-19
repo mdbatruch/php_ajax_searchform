1bfd45bcf7837bab488f86141a5c1eaa9cb192bf
mdbatruch <mdbatruch@gmail.com>
2019-07-18 23:30:58 -0400

    db credentials

---
+++ db_credentials_template.php
@@ -0,0 +1,8 @@
+<?php 
+
+    define("DB_HOST", 'enter host here');
+    define("DB_USER", 'enter user here');
+    define("DB_PASS", 'enter pass here');
+    define("DB_NAME", 'enter db name here');
+
+?>
